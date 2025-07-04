9b376bfc2124d772a4fed30b59368d4395c7f211
Ana Lallena Arquillo <lallena.ana@gmail.com>
2025-07-04 17:18:28 +0200

    fix path for dotfile

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # dotfiles
+
+# Setup new laptop
+1. Install Homebrew
+   ```bash
+   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+   ```
+   
+2. Install 1password
+
+3. Install sublime
+
+4. Create ssh key
--- setup/mac_setup.sh
+++ setup/mac_setup.sh
@@ -1,7 +1,7 @@
 #pushd $HOME > /dev/null
 echo "Adding files"
-ln -s $HOME/ws/personal/dotfiles/shell/DOTbash_profile .bash_profile
-ln -s $HOME/ws/personal/dotfiles/shell/DOTinputrc .inputrc
-ln -s $HOME//ws/personal/dotfiles/git/DOTgitconfig .gitconfig
-ln -s $HOME/ws/personal/dotfiles/shell/DOTbashrc .bashrc
+ln -s $HOME/ws/personal/dotfiles/shell/DOTbash_profile $HOME/.bash_profile
+ln -s $HOME/ws/personal/dotfiles/shell/DOTinputrc $HOME/.inputrc
+ln -s $HOME//ws/personal/dotfiles/git/DOTgitconfig $HOME/.gitconfig
+ln -s $HOME/ws/personal/dotfiles/shell/DOTbashrc $HOME/.bashrc
 #popd > /dev/null
